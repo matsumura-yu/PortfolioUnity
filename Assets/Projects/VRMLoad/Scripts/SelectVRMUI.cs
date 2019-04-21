c4b52a5484eb4afe0c5a69621bd36c3cba1a801c
matsumura-yu <matsumura.yu@isid.co.jp>
2019-04-21 13:14:53 +0900

    Add feature select and load VRM

--- Assets/Projects/VRMLoad/Scripts/SelectVRMUI.cs
+++ Assets/Projects/VRMLoad/Scripts/SelectVRMUI.cs
@@ -2,19 +2,81 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using VRM;
+using VRM.Samples;
+using System.IO;
+using System;
 
 public class SelectVRMUI : MonoBehaviour
 {
-
+    [SerializeField]
+    Button m_button;
     // Start is called before the first frame update
     void Start()
     {
-        
+        m_button.onClick.AddListener(OnOpenClicked);
     }
 
-    // Update is called once per frame
-    void Update()
+    async void OnOpenClicked()
     {
-        
+        var path = FileDialogForWindows.FileDialog("VRMを選択してください", "vrm");
+
+        if (string.IsNullOrEmpty(path))
+        {
+            return;
+        }
+        Debug.Log(path);
+        // 小文字に直して大文字と小文字の判別を無視する
+        var file_extention = Path.GetExtension(path).ToLower();
+        if(file_extention == ".vrm")
+        {
+            // VRMを読み込む機能の提供
+            var context = new VRMImporterContext();
+            // ファイルをByte配列に読み込みファイルを閉じる
+            var bytes = File.ReadAllBytes(path);
+            // GLB形式でJSONを取得しParseする
+            context.ParseGlb(bytes);
+            // metaデータの取得
+            // 引数にサムネイルの取得設定を行える
+            var meta = context.ReadMeta(false);
+
+            Debug.LogFormat("meta_title: {0}", meta.Title);
+
+            // LoadAsync後に引数のメソッドが呼び出されるように登録
+            // context.LoadAsync(_ => OnLoaded(context));
+            // 今UnityのVersionだとObsoleteになっている
+
+            try
+            {
+                // awaitを使いたいのでメソッドにasyncを付ける
+                await context.LoadAsyncTask();
+
+                // 
+                context.EnableUpdateWhenOffscreen();
+
+                OnLoaded(context);
+            }
+            catch (Exception e)
+            {
+                Debug.LogError(e);
+                // 関連するリソースを破棄する
+                context.Dispose();
+                throw;
+            }
+        }
+        else
+        {
+            Debug.LogWarningFormat("unknown file type: {0}", path);
+        }
+    }
+
+    public void OnLoaded(VRMImporterContext context)
+    {
+        // BuildHierarchyメソッドが内部で呼び出されRootの中にVRMGameObject生成
+        var root = context.Root;
+        root.transform.position = new Vector3(0, 0, 0);
+
+        // メッシュを表示する
+        context.ShowMeshes();
     }
 }
